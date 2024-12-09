d26f688b895f2f659f8b998a395113f1c83bfb5b
illusionarydream <illusionary@sjtu.edu.cn>
2024-12-09 14:27:24 +0800

    grid gradient build

--- Fluid_Engine/grid_system/grid.py
+++ Fluid_Engine/grid_system/grid.py
@@ -2,170 +2,76 @@ import taichi as ti
 
 
 @ ti.data_oriented
-class Grid:
-    def __init__(self, resolution: int = 20):
+class Grid2D:
+    def __init__(self, res_x: int = 60, res_y: int = 40):
+        self.res_x = res_x
+        self.res_y = res_y
+        self.dx = 1/res_x
+        self.dy = 1/res_y
 
-        # basic grid information
-        self.resolution = resolution
-        self.cell_size = resolution ** 3
-        self.mesh_size = (resolution + 1) * resolution ** 2
-        self.dd = 1 / resolution
+        # velocity field: staggered grid
+        self.velocity_x = ti.field(dtype=ti.f32, shape=(res_x + 1, res_y))
+        self.velocity_y = ti.field(dtype=ti.f32, shape=(res_x, res_y + 1))
 
-        # cell data
-        self.density = ti.field(dtype=ti.f32, shape=(
-            resolution, resolution, resolution))
-        self.pressure = ti.field(dtype=ti.f32, shape=(
-            resolution, resolution, resolution))
+        # boundary condition
+        self.boundary_type = "Dirichlet"
 
-        # mesh data
-        self.U_velocity = ti.Vector.field(3, dtype=ti.f32, shape=(
-            resolution + 1, resolution, resolution))
-        self.V_velocity = ti.Vector.field(3, dtype=ti.f32, shape=(
-            resolution, resolution + 1, resolution))
-        self.W_velocity = ti.Vector.field(3, dtype=ti.f32, shape=(
-            resolution, resolution, resolution + 1))
+        # pressure field
+        self.pressure = ti.field(dtype=ti.f32, shape=(res_x, res_y))
 
-        self.U_gradient = ti.Vector.field(3, dtype=ti.f32, shape=(
-            resolution, resolution, resolution))
-        self.V_gradient = ti.Vector.field(3, dtype=ti.f32, shape=(
-            resolution, resolution, resolution))
-        self.W_gradient = ti.Vector.field(3, dtype=ti.f32, shape=(
-            resolution, resolution, resolution))
-
-        self.U_laplacian = ti.Vector.field(3, dtype=ti.f32, shape=(
-            resolution, resolution, resolution))
-        self.V_laplacian = ti.Vector.field(3, dtype=ti.f32, shape=(
-            resolution, resolution, resolution))
-        self.W_laplacian = ti.Vector.field(3, dtype=ti.f32, shape=(
-            resolution, resolution, resolution))
-
-    # * helper functions
-    @ ti.func
-    def get_ijk(self, idx: int) -> ti.Vector:
-        i = idx // (self.resolution * self.resolution)
-        j = (idx // self.resolution) % self.resolution
-        k = idx % self.resolution
-        return ti.Vector([i, j, k])
-
+    # * gradient operation
     @ ti.kernel
-    def get_cell_position(self, position: ti.template()):
-
-        for idx in range(self.cell_size):
-
-            ijk = self.get_ijk(idx)
-
-            position[idx] = ijk * self.dd
-
-    @ ti.kernel
-    def grid_to_particle(self, position: ti.template(), color: ti.template()):
-
-        for idx in range(self.cell_size):
-
-            ijk = self.get_ijk(idx)
-
-            position[idx] = ijk * self.dd + 0.5 * self.dd
-            color[idx] = ijk * self.dd + 0.5 * self.dd
-
-    # * gradient functions
-    @ ti.kernel
-    def get_property_gradient(self, property: ti.template(), gradient_x: ti.template(), gradient_y: ti.template(), gradient_z: ti.template(), bound_property: ti.template()):
-        # ! cell data to mesh gradient
-        for idx in range(self.mesh_size):
-
-            i, j, k = self.get_ijk(idx)
-
-            # x gradient
-            if i == self.resolution:
-                gradient_x[i, j, k] = (
-                    bound_property - property[i - 1, j, k]) / self.dd
+    def gradient(self, field: ti.template(), field_bound: float, grad_x: ti.template(), grad_y: ti.template()):
+        # grid cenroid data -> grid edge gradient data
+        for i, j in grad_x:
+            if i > 0 and i < self.res_x:
+                grad_x[i, j] = (field[i, j] - field[i - 1, j]) / self.dx
             elif i == 0:
-                gradient_x[i, j, k] = (
-                    property[i, j, k] - bound_property) / self.dd
+                grad_x[i, j] = (field[i, j] - field_bound) / self.dx
             else:
-                gradient_x[i, j, k] = (
-                    property[i, j, k] - property[i - 1, j, k]) / self.dd
+                grad_x[i, j] = (field_bound - field[i - 1, j]) / self.dx
 
-            # y gradient
-            if i == self.resolution:
-                gradient_y[k, i, j] = (
-                    bound_property - property[k, i - 1, j]) / self.dd
-            elif i == 0:
-                gradient_y[k, i, j] = (
-                    property[k, i, j] - bound_property) / self.dd
+        for i, j in grad_y:
+            if j > 0 and j < self.res_y:
+                grad_y[i, j] = (field[i, j] - field[i, j - 1]) / self.dy
+            elif j == 0:
+                grad_y[i, j] = (field[i, j] - field_bound) / self.dy
             else:
-                gradient_y[k, i, j] = (
-                    property[k, i, j] - property[k, i - 1, j]) / self.dd
+                grad_y[i, j] = (field_bound - field[i, j - 1]) / self.dy
 
-            # z gradient
-            if i == self.resolution:
-                gradient_z[j, k, i] = (
-                    bound_property - property[j, k, i - 1]) / self.dd
-            elif i == 0:
-                gradient_z[j, k, i] = (
-                    property[j, k, i] - bound_property) / self.dd
-            else:
-                gradient_z[j, k, i] = (
-                    property[j, k, i] - property[j, k, i - 1]) / self.dd
-
-    # * laplacian functions
     @ ti.kernel
-    def get_property_laplacian(self, property_x: ti.template(), property_y: ti.template(), property_z: ti.template(),
-                               laplacian_x: ti.template(), laplacian_y: ti.template(), laplacian_z: ti.template()):
-        # ! mesh data to cell laplacian
-        for idx in range(self.mesh_size):
-
-            i, j, k = self.get_ijk(idx)
-
-            # ! apply dirichlet boundary condition
-            xn00, xp00, x0n0, x0p0, x00n, x00p = 0, 0, 0, 0, 0, 0
-            yn00, yp00, y0n0, y0p0, y00n, y00p = 0, 0, 0, 0, 0, 0
-            zn00, zp00, z0n0, z0p0, z00n, z00p = 0, 0, 0, 0, 0, 0
-
-            if i > 0:
-                xn00 = property_x[i - 1, j, k]
-                y0n0 = property_y[k, i - 1, j]
-                z00n = property_z[j, k, i - 1]
-            if i < self.resolution:
-                xp00 = property_x[i + 1, j, k]
-                y0p0 = property_y[k, i + 1, j]
-                z00p = property_z[j, k, i + 1]
-            if j > 0:
-                x0n0 = property_x[i, j - 1, k]
-                y00n = property_y[k, i, j - 1]
-                zn00 = property_z[j - 1, k, i]
-            if j < self.resolution:
-                x0p0 = property_x[i, j + 1, k]
-                y00p = property_y[k, i, j + 1]
-                zp00 = property_z[j + 1, k, i]
-            if k > 0:
-                x00n = property_x[i, j, k - 1]
-                yn00 = property_y[k - 1, i, j]
-                z0n0 = property_z[j, k - 1, i]
-            if k < self.resolution:
-                x00p = property_x[i, j, k + 1]
-                yp00 = property_y[k + 1, i, j]
-                z0p0 = property_z[j, k + 1, i]
-
-            # x laplacian
-            laplacian_x[i, j, k] = (xp00 + xn00 + y0p0 + y0n0 + z00p + z00n -
-                                    6 * property_x[i, j, k]) / (self.dd ** 2)
-            # y laplacian
-            laplacian_y[k, i, j] = (x0p0 + x0n0 + yp00 + yn00 + zp00 + zn00 -
-                                    6 * property_y[k, i, j]) / (self.dd ** 2)
-            # z laplacian
-            laplacian_z[j, k, i] = (x00p + x00n + y00p + y00n + z0p0 + z0n0 -
-                                    6 * property_z[j, k, i]) / (self.dd ** 2)
-    # * update
-
-    def update(self):
-        pass
+    def SetField(self, field: ti.template()):
+        for i, j in field:
+            field[i, j] = i * self.dx
 
 
-if __name__ == '__main__':
+# * tools
+@ ti.kernel
+def Field_to_Pixel(field: ti.template(), pixel: ti.template()):
+    for i, j in pixel:
+        pixel[i, j] = field[i, j]
 
+
+if __name__ == "__main__":
     ti.init(arch=ti.gpu)
 
-    grid = Grid(5)
+    grid = Grid2D(800, 600)
+    grid.SetField(grid.pressure)
 
-    position = ti.Vector.field(3, dtype=ti.f32, shape=(5**3))
-    grid.get_cell_position(position=position)
+    # Initialize GUI
+    gui_shape = (grid.res_x, grid.res_y)
+    gui = ti.GUI("2D Grid", res=gui_shape)
+
+    # Create a pixel field to display the pressure field
+    pixel_field = ti.field(dtype=ti.f32, shape=gui_shape)
+
+    while gui.running:
+        # Update the gradient
+        grid.gradient(grid.pressure, 0, grid.velocity_x, grid.velocity_y)
+
+        # Convert the pressure field to pixel values
+        Field_to_Pixel(grid.velocity_y, pixel_field)
+
+        # Display the pixel field
+        gui.set_image(pixel_field)
+        gui.show()
